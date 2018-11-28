6c44914ba5187d36a89a9605a621c7454336f162
Hayden Liao <liao21@up.edu>
2018-11-28 15:35:18 -0800

    Created liao21 function for favorite food.

---
+++ liao21.c
@@ -0,0 +1,11 @@
+/* this is file liao21.c */
+
+#include <stdio.h>
+#include "liao21.h"
+
+void username() {
+
+	printf("***************************************");
+	printf("Hayden likes potstickers and tacos.\n");
+	printf("***************************************");
+}
